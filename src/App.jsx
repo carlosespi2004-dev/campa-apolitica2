6ba4786cbf31c16e077a6ee3e5b61b990ae4a6c5
Carlos Espinola <carlosespi2004l@gmail.com>
2026-03-23 16:22:56 -0300

    barrio a futuros votantes

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function GreenHeart() {
     <img
       src={logocarmona}
       alt="Logo Carmona"
-      style={{ width: "60px", height: "40px", borderRadius: "30px" }}
+      style={{ width: "70px", height: "40px", borderRadius: "30px" }}
     />
   );
 }
